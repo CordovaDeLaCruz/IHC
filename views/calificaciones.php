16e9cf8d5445a204be735edc5fcda4937f3084f6
Jorge <yarsel96@gmail.com>
2019-05-31 00:55:11 -0500

    Vista Calificaciones

---
+++ views/calificaciones.php
@@ -0,0 +1,81 @@
+<div id="content_calificaciones">
+	<div id="calif_cursos">
+		<h1>Cursos: 2019 - I</h1>
+		<div class="content_curso">
+			<p class="nombre_curso">Interacción Hombre - Computador</p>
+			<p class="">Hace 4 días</p>
+		</div>
+		<div class="content_curso">
+			<p class="nombre_curso">Metodología de la investigación</p>
+			<p class="">Hace 6 días</p>
+		</div>
+		<div class="content_curso">
+			<p class="nombre_curso">Gestión de la configuración</p>
+			<p class="">Hace 6 días</p>
+		</div>
+		<div class="content_curso">
+			<p class="nombre_curso">Gerencia de proyectos de software</p>
+			<p class="">Hace 8 días</p>
+		</div>
+	</div>
+
+	<div id="calif_descri">
+		<h1>Tareas: Interacción Hombre - Computador</h1>
+		<div class="tarea">
+			<div class="descrip_tarea">
+				<h4>TALLER 1</h4>
+				<p>Se realiza una encuesta para conocer a los usuarios finales del sistema. La encuesta debe tener preguntas que apoyen el análisis para la elaboración de los prototipos y posteriormente el diseño de la interfaz gráfica del sistema.</p>	
+			</div>
+			<div class="caract_tarea">
+				<div class="estado">
+					<div>ESTADO:</div>
+					<div id="entregado">Entregado</div>
+				</div>
+				<div class="nota">
+					<div>NOTA:</div>
+					<div>
+						<div>14</div>
+					</div>
+				</div>	
+			</div>
+			
+		</div>
+		<div class="tarea">
+			<div class="descrip_tarea">
+				<h4>PRIMER TRABAJO DE INVESTACIÓN</h4>
+				<p>El alumno debe investigar sobre modelo mental, diseño centrado en el usuario y retos de las interfaces para las nuevas tecnologías. El alumno deberá exponer su investigación en la siguiente clase (no hay otra fecha de presentación).</p>	
+			</div>
+			<div class="caract_tarea">
+				<div class="estado">
+					<div>ESTADO:</div>
+					<div id="no_entregado">No Entregado</div>
+				</div>
+				<div class="nota">
+					<div>NOTA:</div>
+					<div>
+						<div>00</div>
+					</div>
+				</div>	
+			</div>
+			
+		</div>
+		<div class="tarea">
+			<div class="descrip_tarea">
+				<h4>TALLER 2</h4>
+				<p>Se debe realizar el diseño de interfaces gráfica en baja y media fidelidad (puede hacer uso de herramientas software). El diseño debe ser acorde al tema entregado en clase. El trabajo se envía por correo y se expone en la siguiente clase. El trabajo se desarrolla en grupos de laboratorio.</p>	
+			</div>
+			<div class="caract_tarea">
+				<div class="estado">
+					<div>ESTADO:</div>
+					<div id="entregado">Entregado</div>
+				</div>
+				<div class="nota">
+					<div>NOTA:</div>
+					<div>
+						<div>16</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+</div>
