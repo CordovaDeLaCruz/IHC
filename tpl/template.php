bc6f8376bc70f108e37d087933cfe4cfeac0a15e
GersonCarrascoL <gersoncarrasco.25.8@gmail.com>
2019-05-31 14:27:20 -0500

    add curso-detalle

--- tpl/template.php
+++ tpl/template.php
@@ -19,6 +19,9 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
+<!-- Material Icons -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="app/css/bootstrap.css"/>
--- views/cursos-detalle.php
+++ views/cursos-detalle.php
@@ -1,47 +1,64 @@
-<div class="contenedor_cursos_detalle col-xs-12">
-    <div class="contenedor_opciones col-xs-5">
-        <div>
-            <ul class="list-group">
-                <li class="list-group-item">Mis anuncios</li>
-                <li class="list-group-item">Bibliografía del Curso</li>
-                <li class="list-group-item">Lecturas</li>
-                <li class="list-group-item">Mis calificaciones</li>
-            </ul>
-        </div>
-        <div>
-            <ul class="list-group">
-                <li class="list-group-item">Semana 1</li>
-                <li class="list-group-item">Semana 2</li>
-                <li class="list-group-item">Semana 3</li>
-                <li class="list-group-item">Semana 4</li>
-                <li class="list-group-item">Semana 5</li>
-                <li class="list-group-item">Semana 6</li>
-                <li class="list-group-item">Semana 7</li>
-                <li class="list-group-item">Semana 8</li>
-                <li class="list-group-item">Semana 9</li>
-                <li class="list-group-item">Semana 10</li>
-                <li class="list-group-item">Semana 11</li>
-                <li class="list-group-item">Semana 12</li>
-            </ul>
-        </div>
-    </div>
-    <div class="contenedor_curso_detalle_vista col-xs-7">
-        <div class="curso_title">
-            <h1>
-                INTERACCION HOMBRE COMPUTADORA
-            </h1>
-        </div>
-        <div class="info_general">
-            <h2>
-                INFORMACION GENERAL
-            </h2>
-        </div>
-        <div class="curso_info">
-            <div class="curso_sillabus">
-                SYLLABUS
+<div class="container">
+    <div class="contenedor_cursos_detalle col-xs-12">
+        <div class="contenedor_opciones col-xs-5">
+            <div>
+                <ul class="list-group">
+                    <li class="list-group-item">Mis anuncios</li>
+                    <li class="list-group-item">Bibliografía del Curso</li>
+                    <li class="list-group-item">Lecturas</li>
+                    <li class="list-group-item">Mis calificaciones</li>
+                </ul>
             </div>
-            <div class="curso_bienvenida">
-                BIENVENIDO AL CURSO
+            <div>
+                <ul class="list-group">
+                    <li class="list-group-item">Semana 1</li>
+                    <li class="list-group-item">Semana 2</li>
+                    <li class="list-group-item">Semana 3</li>
+                    <li class="list-group-item">Semana 4</li>
+                    <li class="list-group-item">Semana 5</li>
+                    <li class="list-group-item">Semana 6</li>
+                    <li class="list-group-item">Semana 7</li>
+                    <li class="list-group-item">Semana 8</li>
+                    <li class="list-group-item">Semana 9</li>
+                    <li class="list-group-item">Semana 10</li>
+                    <li class="list-group-item">Semana 11</li>
+                    <li class="list-group-item">Semana 12</li>
+                </ul>
+            </div>
+        </div>
+        <div class="contenedor_curso_detalle_vista col-xs-7">
+            <div class="curso_title">
+                <h1>
+                    INTERACCION HOMBRE COMPUTADORA
+                </h1>
+            </div>
+            <div class="info_general_detalle">
+                <h2>
+                    INFORMACION GENERAL
+                </h2>
+            </div>
+            <div class="curso_info_detalle">
+                <div class="curso_sillabus">
+                    <div>
+                        <i class="material-icons md-36">assignment</i>
+                    </div><a href="">
+                        <h2>SYLLABUS</h2>
+                    </a>
+                </div>
+                <div class="curso_contenido">
+                    <div class="curso_bienvenida center-block">
+                        <div>
+                            <i class="material-icons md-36">local_library</i>
+                        </div>
+                        <h2>BIENVENIDO AL CURSO</h2>
+                    </div>
+                    <div class="curso_bienvenida_cuerpo">
+                        <p>
+                        Me da mucho gusto darles la bienvenida a este nuevo curso en la Universidad. Para la mayoría, se trata de la continuidad de sus estudios universitarios; para otros, es el último tramo de un itinerario que está a punto de concluir y, para unos más, es el inicio de un recorrido exigente y que les supondrá su mayor esfuerzo. Quiero aprovechar este breve mensaje de bienvenida para recordarles que la universidad —el proyecto que cada una y cada uno de ustedes han decidido acometer— es en la medida de lo que sus estudiantes aporten, mediante el diálogo y la constante circulación del pensamiento.
+                        </p>
+                        <img src="../app/img/logo_ihc.png" alt="">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
