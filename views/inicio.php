b6ce84170094770caa781ac9bc10b7fa73ed1ed5
CordovaDeLaCruz <erickcd57@gmail.com>
2019-05-31 10:56:34 -0500

    Mi facultad

---
+++ views/inicio.php
@@ -0,0 +1,44 @@
+<div>
+    <div id="content_pri">
+        <div id="mis_cursos">
+            <h1>Mis Cursos: Vista Rápida</h1>
+            <hr id="linea">
+            <h4>Interacción Hombre - Computador</h4>
+            <h4>Metodología de la Investigación</h4>
+            <h4>Gestión de la Configuración y Mantenimiento</h4>
+            <h4>Gerencia de Proyectos de Software</h4>
+        </div>
+        <div id="mis_anuncios">
+            <h1>Mis Anuncios</h1>
+            <hr id="linea">
+            <p>Un nuevo triunfo para el Perú lo traen nuestros alumnos de la FISI, Carlos Ramirez, 
+                Luigi Castillo y Robert Gutierrez, asesorados por la Dra. Lenis Wong Portillo,
+                que ocuparon el...
+                <a href="https://www.facebook.com/102069853667291/posts/346043775936563/" target="_blank">leer más</a>
+            </p>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+    <div id="content_sec">
+        <div id="mis_organizaciones">
+            <h1>Mis Organicaciones</h1>
+            <hr id="linea">
+            <h5>No se encuentra en ninguna oranigación...</h5>
+            <br>
+            <br>
+            <br>
+            <br>
+        </div>
+        <div id="mis_tareas">
+            <h1>Mis Tareas</h1>
+            <hr id="linea">
+            <h5>No hay tareas pendientes...</h5>
+            <br>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+</div>
