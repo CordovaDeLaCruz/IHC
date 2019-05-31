3010aa7545099dfe93f3e15b49797d70710aa935
joel <joel.bejar.mallma@gmail.com>
2019-05-30 23:31:18 -0500

    vista mensajes

--- tpl/template.php
+++ tpl/template.php
@@ -12,16 +12,26 @@
   <link href='https://fonts.googleapis.com/css?family=Lato:400,300,100,700,900' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="app/css/bxslider.css"/>
   <link rel="stylesheet" href="app/css/fancybox.css"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="app/css/bootstrap.css"/>
   <link rel="stylesheet" href="app/css/demo.css"/>
   <link rel="stylesheet" href="app/css/style.css"/>
-   
+
   <script src="app/js/jquery.js"></script>
   <script src="app/js/fancybox.js"></script>
   <script src="app/js/bootstrap.js"></script>
   <script src="app/js/bxslider.js"></script>
   <script src="app/js/parallax.js"></script>
   <script src="app/js/app.js"></script>
+
 </head>
 <body>
  
