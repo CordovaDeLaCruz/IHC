7f106c2e134561ee4b669b1c60edce4c1d6a12ff
GersonCarrascoL <gersoncarrasco.25.8@gmail.com>
2019-05-31 01:03:15 -0500

    Finish module login

---
+++ views/login.php
@@ -0,0 +1,42 @@
+
+    <section id="login_view">
+        <div class="panel_login">
+            <div class="col-md-12">
+                <div class="container">
+                    <div class="panel_container">
+                        <div class="panel_left col-md-3 col-md-offset-2">
+                            <div class="panel_left_title">
+                                BIENVENIDOS AL AULA VIRTUAL DE LA FACULTAD DE INGENIERIA DE SISTEMAS E INFORMATICA
+                            </div>
+                            <div class="panel_left_icons">
+                                <img src="app/img/logo_web.png" alt="">
+                                <img src="app/img/fb_logo.png" alt="">
+                            </div>
+                        </div>
+                        <div class="panel_right col-md-5">
+                            <div class="panel_right_logo">
+                                <img class="img-responsive logo_login center-block" src="app/img/logo_fisi.png" alt="">
+                            </div>
+                            <div class="panel_right_form">
+                                <div class="col-md-10 col-md-offset-1">
+                                    <form action="">
+                                        <div class="form-group input_login">
+                                            <input type="email" class="form-control input_login" id="exampleInputEmail1" placeholder="Email">
+                                        </div>
+                                        <div class="form-group input_login">
+                                            <input type="password" class="form-control input_login" id="exampleInputPassword1" placeholder="Contraseña">
+                                        </div>
+                                        <div class="form-group center-block">
+                                            <a href="inicio.php">
+                                                <input type="button" class="btn btn-default button_login center-block" value="Acceder">
+                                            </a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
